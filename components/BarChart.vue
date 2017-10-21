3917ec67ded226dc88763db2e4318f56a645242d
Ronnie <rcamacho@simpleviewinc.com>
2017-10-21 15:22:15 -0700

    Latest FrontEnd changes

--- components/BarChart.vue
+++ components/BarChart.vue
@@ -1,23 +1,23 @@
 <script>
 // lineChart.js
-import { Line, mixins } from 'vue-chartjs'
+import { Bar, mixins } from 'vue-chartjs'
 const { reactiveProp } = mixins
 
 export default {
-  extends: Line,
+  extends: Bar,
   mixins: [reactiveProp],
   props: ['options'],
   mounted () {
     this.gradient = this.$refs.canvas.getContext('2d').createLinearGradient(0, 0, 0, 450)
     this.gradient2 = this.$refs.canvas.getContext('2d').createLinearGradient(0, 0, 0, 450)
 
-    this.gradient.addColorStop(0, 'rgba(248, 107, 79, 0.5)')
-    this.gradient.addColorStop(0.5, 'rgba(248, 107, 790, 0.25)');
-    this.gradient.addColorStop(1, 'rgba(248, 107, 790, 0)');
+    this.gradient.addColorStop(0, 'rgba(255, 0,0, 0.5)')
+    this.gradient.addColorStop(0.5, 'rgba(255, 0, 0, 0.25)');
+    this.gradient.addColorStop(1, 'rgba(255, 0, 0, 0)');
 
-    this.gradient2.addColorStop(0, 'rgba(95, 174, 227, 0.9)')
-    this.gradient2.addColorStop(0.5, 'rgba(95, 174, 227, 0.25)');
-    this.gradient2.addColorStop(1, 'rgba(95, 174, 227, 0)');
+    this.gradient2.addColorStop(0, 'rgba(0, 231, 255, 0.9)')
+    this.gradient2.addColorStop(0.5, 'rgba(0, 231, 255, 0.25)');
+    this.gradient2.addColorStop(1, 'rgba(0, 231, 255, 0)');
     // this.chartData is created in the mixin.
     // If you want to pass options please create a local options object
     // this.renderChart(this.chartData, this.options)
--- pages/index.vue
+++ pages/index.vue
@@ -24,25 +24,46 @@
             <v-list-tile-title>Settings</v-list-tile-title>
           </v-list-tile-content>
         </v-list-tile>
+        <v-list-tile @click="">
+          <v-list-tile-action>
+            <v-icon>trending_up</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title>Most Popular</v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+        <v-list-tile @click="">
+          <v-list-tile-action>
+            <v-icon>help</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title>Help</v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
       </v-list>
     </v-navigation-drawer>
     <v-toolbar app fixed clipped-left>
       <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
-      <v-toolbar-title>Application</v-toolbar-title>
+      <v-toolbar-title>
+        <img src="~/assets/images/logo.png" alt="" class="dashboard-logo">
+      </v-toolbar-title>
     </v-toolbar>
     <main>
       <v-content>
         <v-container fluid fill-height>
           <v-layout justify-center align-center>
-            <v-tooltip right>
-              <v-btn icon large :href="source" target="_blank" slot="activator">
-                <v-icon large>code</v-icon>
-              </v-btn>
-              <span>Source</span>
-            </v-tooltip>
-            <line-chart 
-            :chart-data="datacollection"></line-chart>
+            <div class="flex xs6">
+              <h4>adWord: Spokane</h4>
+              <line-chart 
+              :chart-data="datacollection"></line-chart>
+            </div>
+            <div class="flex xs6">
+              <bar-chart 
+            :chart-data="datacollection"></bar-chart>
+            </div>
+            
           </v-layout>
+         
         </v-container>
       </v-content>
     </main>
@@ -55,23 +76,41 @@
 <script>
   import Vue from 'vue'
   import Vuetify from 'vuetify'
-  import LineChart from '~/components/LineChart.vue'
   import('~/node_modules/vuetify/dist/vuetify.min.css')
+  import LineChart from '~/components/LineChart.vue'
+  import BarChart from '~/components/BarChart.vue'
+  
  
   Vue.use(Vuetify)
   export default {
     components: {
-      LineChart
+      LineChart,
+      BarChart
     },
     data () {
+
       return {
+        drawer: true,
         datacollection: {
-          labels: ['January', 'February', 'January', 'February', 'January', 'February'],
+          labels: ['January', 'February', 'March', 'April', 'June', 'July', 'Agust', 'Septemeber', 'October', 'November', 'December'],
           datasets: [
             {
               label: 'Data One',
-              backgroundColor: '#f87979',
-              data: [80, 0]
+              borderColor: '#FC2525',
+              pointBackgroundColor: 'white',
+              borderWidth: 1,
+              pointBorderColor: 'white',
+              backgroundColor: this.gradient,
+              data: [40, 39, 10, 40, 39, 80, 40]
+            },
+            {
+              label: 'Data Two',
+              borderColor: '#05CBE1',
+              pointBackgroundColor: 'white',
+              pointBorderColor: 'white',
+              borderWidth: 1,
+              backgroundColor: this.gradient2,
+              data: [60, 55, 32, 10, 2, 12, 53]
             }
           ]
         }
@@ -82,3 +121,14 @@
     }
   }
 </script>
+
+<style>
+  .dashboard-logo{
+    line-height: 1;
+    max-width: 200px;
+  }
+  .toolbar__title{
+    line-height: 1;
+  }
+
+</style>
